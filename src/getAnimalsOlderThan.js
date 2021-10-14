bdcdb19bda389e7359285ae9ddf4319f20a4b0b2
Angelica <angelicapedroso05@gmail.com>
2021-10-14 01:14:51 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -8,10 +8,15 @@
 
 // Ao passar o nome de uma espécie e uma idade, testa se todos os animais desta espécie possuem a idade mínima especificada.
 
+const {
+  species,
+} = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return species.find((specie) => specie.name === animal)
+    .residents.every((resident) => resident.age > age);
 }
+console.log(getAnimalsOlderThan('otters', 7));
 
 module.exports = getAnimalsOlderThan;
