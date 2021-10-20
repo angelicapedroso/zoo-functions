08e22fc26e125bc5cae2a08b1bc90b190407afb5
Angelica <angelicapedroso05@gmail.com>
2021-10-20 11:31:11 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
+const {
+  species,
+} = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return species.reduce((acc, value) => {
+      acc[value.name] = value.residents.length;
+      return acc;
+    }, {});
+  }
+  if (!animal.sex) {
+    return species.find((valueSpecie) =>
+      valueSpecie.name === animal.specie).residents.length;
+  }
+  return species.find((valueSpecie) =>
+    valueSpecie.name === animal.specie).residents
+    .filter((valueSex) => valueSex.sex === animal.sex).length;
 }
+
+console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
 
 module.exports = countAnimals;
