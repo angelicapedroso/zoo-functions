bd0284c3a8f8d363a60809c70048320e2c6693aa
Angelica <angelicapedroso05@gmail.com>
2021-10-20 17:39:42 -0300

    requisito 8

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
+const {
+  employees,
+  species,
+} = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const firstSpecie = employees.find((employee) => employee.id === id).responsibleFor[0];
+  const speciesResidents = species.find((specie) => specie.id === firstSpecie).residents;
+  const olderAnimal = speciesResidents.reduce((acc, animal) => (
+    acc.age > animal.age ? acc : animal
+  ), 0);
+  return [olderAnimal.name, olderAnimal.sex, olderAnimal.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
